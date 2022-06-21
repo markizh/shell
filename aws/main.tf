fce159c4ffce55c90f640bd857f2af95da77f177
needforyou <admin@vipuser.site>
2022-06-21 12:22:34 +0800

    create EC2 via terraform

--- aws/main.tf
+++ aws/main.tf
@@ -1,22 +1,44 @@
-#provider "aws" {
-#  region = " ap-southeast-1"
-#  access_key = ""
-#  secret_key = ""
-#}
+# 引用变量
+# 创建aws云基础设施 https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+provider "aws" {
+  region = var.region
+}
 
-# 创建vpc
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+# 创建aws ec2实例
+resource "aws_instance" "api" {
+  # lookup通过查询定义的变量获取值
+  ami = lookup(var.ami, var.region)
+  instance_type = var.instance_type
+  key_name = aws_key_pair.ssh.key_name
+  // EC2实例创建后运行定义脚本
+  #user_data = "docker.sh"
   tags = {
-    Name="prod_vpc"
+    Name  = "api"
   }
 }
 
-# 创建子网
-resource "aws_subnet" "subnet-1a" {
-  cidr_block = "10.0.0.0/20"
-  vpc_id     = aws_vpc.main.id
-  tags = {
-    Name="prod_subnet_1a"
+# 添加ssh公钥
+resource "aws_key_pair" "ssh" {
+    key_name = "ap-southeast-1-key1"
+    // file读取文件以字符串方式返回文件内容
+    public_key = file(var.public_key)
+}
+
+# 通过data数据源获取信息
+data "aws_security_group" "default" {
+  filter {
+    name   = "group-name"
+    values = ["default"]
   }
+}
+
+# 开放安全组规则
+resource "aws_security_group_rule" "ssh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["103.48.141.80/32"]
+    # 引用数据源中的变量值
+    security_group_id = data.aws_security_group.default.id
 }
